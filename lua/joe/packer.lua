39d1feb236efd30e3b92dae50ecaeffb7c81b5b7
Joe Miller <joemillervi@gmail.com>
2023-10-15 23:37:48 -0400

    add colors, need terminal bg, add zenmode

--- lua/joe/packer.lua
+++ lua/joe/packer.lua
@@ -1,58 +1,63 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-    use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.2',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
-    }
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.2",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-    use {
-        "nvim-telescope/telescope-file-browser.nvim",
-        requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
-    }
+	use({
+		"nvim-telescope/telescope-file-browser.nvim",
+		requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	})
 
-    use({
-        'rose-pine/neovim',
-        as = 'rose-pine',
-        config = function() vim.cmd('colorscheme rose-pine') end
-    })
+	use({
+		"rose-pine/neovim",
+		as = "rose-pine",
+		config = function()
+			vim.cmd("colorscheme rose-pine")
+		end,
+	})
 
-    use({ 'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' } })
-    use('nvim-treesitter/playground')
+	use({ "nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" } })
+	use("nvim-treesitter/playground")
 
-    use('ThePrimeagen/harpoon')
+	use("ThePrimeagen/harpoon")
 
-    use('mbbill/undotree')
+	use("mbbill/undotree")
 
-    use('tpope/vim-fugitive')
+	use("tpope/vim-fugitive")
 
-    use {
-      "folke/trouble.nvim",
-      requires = {"nvim-tree/nvim-web-devicons"}
-    }
+	use({
+		"folke/trouble.nvim",
+		requires = { "nvim-tree/nvim-web-devicons" },
+	})
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },   -- Required
-            { 'williamboman/mason.nvim' }, -- Optional
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{ "williamboman/mason.nvim" }, -- Optional
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' }, -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'L3MON4D3/LuaSnip' }, -- Required
-        }
-    }
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "L3MON4D3/LuaSnip" }, -- Required
+		},
+	})
 
-    use 'mhartington/formatter.nvim'
+	use("mhartington/formatter.nvim")
+
+	use("folke/zen-mode.nvim")
 end)
